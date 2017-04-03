f15577164a37bacb4fc1d3974aab6a9e58129049
Mango N <mangesh747@gmail.com>
2017-04-03 19:42:34 -0400

    Began work on hand scaling

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var cat_hand;
+var angle = 75;
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
@@ -6,14 +7,24 @@ function setup() {
 }
 
 function draw() {
-	background(220);
-	noStroke();
+	background(255); // redraw background every frame
+	noStroke(); // no outline on the objects
+	console.log(scaleCatArm(angle));
 	cat_hand.display();
 }
 
 function MovingCatHand(){
 	this.display = function(){
+		//draw orange color on the quadrilateral cat arm
+		fill('orange');
+		quad((mouseX - 125) - scaleCatArm(angle), 0, mouseX - scaleCatArm(angle), 0, mouseX + 50, window.innerHeight/5, mouseX - 50, window.innerHeight/5); // cat arm is 125 px thick
+		//draw off-white color on the elliptical cat paw
+		fill(220);
 		ellipse(mouseX, window.innerHeight/5, 100, 100);
-		quad(0, 0, 200, 0, mouseX + 50, window.innerHeight/5, mouseX - 50, window.innerHeight/5);
 	}
+}
+
+function scaleCatArm(angle_constant){
+	var scaledAngle = angle_constant * ((window.innerWidth - mouseX)/1000);
+	return (window.innerHeight / Math.tan(scaledAngle));
 }
